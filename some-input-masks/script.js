fa041e72b6067ac8b1b96f262c7e9ac1edb4a240
Felipe Gueller da Silva Gomes <felipe.gueller@gmail.com>
2021-11-30 10:21:41 -0300

    feat: learning regex

---
+++ some-input-masks/script.js
@@ -0,0 +1,41 @@
+const masks = {
+  cpf(value) {
+    return value
+      .replace(/\D/g, '')
+      .replace(/(\d{3})(\d)/, '$1.$2')
+      .replace(/(\d{3})(\d)/, '$1.$2')
+      .replace(/(\d{3})(\d{1,2})/, '$1-$2')
+      .replace(/(-\d{2})\d+?$/, '$1')
+  },
+  cnpj(value) {
+    return value
+      .replace(/\D/g, '')
+      .replace(/(\d{2})(\d)/, '$1.$2')
+      .replace(/(\d{3})(\d)/, '$1.$2')
+      .replace(/(\d{3})(\d)/, '$1/$2')
+      .replace(/(\d{4})(\d)/, '$1-$2')
+      .replace(/(-\d{2})\d+?$/, '$1')
+  },
+  telefone(value) {
+    return value
+      .replace(/\D/g, '')
+      .replace(/(\d{2})(\d)/, '($1) $2')
+      .replace(/(\d{4})(\d)/, '$1-$2')
+      .replace(/(\d{4})-(\d)(\d{4})/, '$1$2-$3')
+      .replace(/(-\d{4})\d+?$/, '$1')
+  },
+  cep(value) {
+    return value
+      .replace(/\D/g, '')
+      .replace(/(\d{5})(\d)/, '$1-$2')
+      .replace(/(-\d{3})\d+?$/, '$1')
+  }
+}
+
+document.querySelectorAll('input').forEach(input => {
+  const field = input.dataset.js
+
+  input.oninput = event => {
+    event.target.value = masks[field](event.target.value)
+  }
+})
