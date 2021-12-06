fc1a507640364edd1d674e178a5efc532326efbc
Felipe Gueller da Silva Gomes <felipe.gueller@gmail.com>
2021-12-06 17:01:58 -0300

    feat: learning about map array method

---
+++ map/index.js
@@ -0,0 +1,13 @@
+// transform array numbers
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const squareNumbers = numbers.map(item => item ** 2)
+console.log('Square numbers: ',squareNumbers)
+
+// Taking some names
+const books = [
+  { name: 'Chapeuzinho vermelho', date: '20/12/1798' },
+  { name: 'Jack and the beanstalk', date: '04/11/1948' },
+  { name: 'Lord of the Rings - The return of the king', date: '05/04/1945' },
+]
+const onlyNames = books.map(({name}) => name)
+onlyNames.forEach(name => console.log(name))
