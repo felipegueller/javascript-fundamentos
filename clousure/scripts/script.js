75df2bd9137ae455262f6f241b9f7cefe981dd5b
Felipe Gueller da Silva Gomes <felipe.gueller@gmail.com>
2022-02-09 10:02:16 -0300

    feat: initing to learn about clousure ideia

---
+++ clousure/scripts/script.js
@@ -0,0 +1,9 @@
+// parei no minuto 11:00
+
+const lexical = 'Oi lexical'
+
+// doing the safe function returns an object
+// because I use the () to wrap the {}
+const safe = secret => ({})
+
+safe()
