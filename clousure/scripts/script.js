55a5b8aaa6850fa0a920a66328243211d88d4fe9
Felipe Gueller da Silva Gomes <felipe.gueller@gmail.com>
2022-02-11 12:32:30 -0300

    learning more about clousure concept

--- clousure/scripts/script.js
+++ clousure/scripts/script.js
@@ -1,9 +1,28 @@
 // parei no minuto 11:00
 
+/* lexical scope is the scope when the function have access to parent's scope, but the parent does not have access to it's scope. */
+
 const lexical = 'Oi lexical'
 
-// doing the safe function returns an object
-// because I use the () to wrap the {}
-const safe = secret => ({})
+// the below function returns an object
+const safe = secret => ({
+  getSecret: () => secret
+})
 
-safe()
+const privateUserData = 'Dado privado'
+const obj = safe(privateUserData)
+console.log('Retorno: ', obj.getSecret())
+
+const func = secret => () => secret
+console.log(func(privateUserData))
+console.log(func(privateUserData)())
+
+const addTo = a => b => a + b
+
+const increment1 = addTo(1)
+const increment10 = addTo(10)
+const increment20 = addTo(20)
+
+console.log(increment1(7))
+console.log(increment10(7))
+console.log(increment20(7))
