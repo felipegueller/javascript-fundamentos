b4e66f402a8d1ca14ad2970540823376e73a5505
Felipe Gueller da Silva Gomes <felipe.gueller@gmail.com>
2022-03-11 17:24:17 -0300

    feat: consuming the josonplaceholder API and send the data to frontend

--- using-api-frontend-and-backend/server.js
+++ using-api-frontend-and-backend/server.js
@@ -1,11 +1,20 @@
 const cors = require('cors')
 const express = require('express')
+const axios = require('axios')
 const app = express()
 
 app.use(cors())
 
-app.get('/', (req, res) => {
-  return res.json([{ name: 'felipe' }, { name: 'alex' }])
+app.get('/', async (req, res) => {
+  try {
+    // extracting the data into the response
+    const { data } = await axios('https://jsonplaceholder.typicode.com/users')
+
+    // send the json data to frontend
+    return res.json(data)
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 app.listen('4567')
