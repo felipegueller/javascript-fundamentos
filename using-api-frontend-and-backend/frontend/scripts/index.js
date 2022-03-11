700db6a09da299c831fa38cac7764a84237fdfa0
Felipe Gueller da Silva Gomes <felipe.gueller@gmail.com>
2022-03-11 17:22:14 -0300

    feat: gettting the API data and putting in frontend list

--- using-api-frontend-and-backend/frontend/scripts/index.js
+++ using-api-frontend-and-backend/frontend/scripts/index.js
@@ -1,11 +1,26 @@
-const accessAPI = async () => {
+const usersList = document.querySelector('ul[data-js="users-list"]')
+
+const getUsers = async () => {
   try {
     const response = await fetch('http://localhost:4567/')
-    const data = await response.json()
-    console.log(data)
+    return response.json() // transform the data to json format
   } catch (error) {
     console.log('error: ', error)
   }
 }
 
-window.addEventListener('DOMContentLoaded', accessAPI)
+const insertUserInList = async () => {
+  const users = await getUsers()
+
+  // transform in one string of LIs elements
+  const lis = users.reduce((accumulator, { name }) => {
+    return (accumulator += `<li>${name}</li>`)
+  }, '')
+
+  // Setting itens in list
+  usersList.innerHTML = lis
+}
+
+insertUserInList()
+
+window.addEventListener('DOMContentLoaded', getUsers)
