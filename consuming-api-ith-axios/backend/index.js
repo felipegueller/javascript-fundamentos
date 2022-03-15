eeaa528ed89eb46066ee75a93be8eafa3d646850
Felipe Gueller da Silva Gomes <felipe.gueller@gmail.com>
2022-03-15 12:05:56 -0300

    feat: configuring the server.js and creating the get by id route

---
+++ consuming-api-ith-axios/backend/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.listen(4567, console.log('The server is running on 4567 door!'))
+
+app.use(express.json())
+app.use(cors())
+
+let users = [
+  {
+    id: 1,
+    name: 'Felipe Gueller',
+    avatar: 'https://avatars.githubusercontent.com/u/35064154?v=4',
+    city: 'Castelo'
+  }
+]
+
+app.route('/api/:id').get((req, res) => {
+  const userId = req.params.id
+  console.log(`Id do usuário: ${userId}`)
+
+  const user = users.find(user => Number(user.id) === Number(userId))
+
+  return !user ? res.json('User not found!') : res.json(user)
+})
