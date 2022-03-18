11755af0a61cad7b41d16202dbdf1d7f8db3be5e
Felipe Gueller da Silva Gomes <felipe.gueller@gmail.com>
2022-03-17 21:00:43 -0300

    feat: adding the update and delete function

--- consuming-api-ith-axios/axios-api/scripts/main.js
+++ consuming-api-ith-axios/axios-api/scripts/main.js
@@ -7,6 +7,12 @@ const newUser = {
   city: 'Las Vegas'
 }
 
+const updateUser = {
+  name: 'Lucky Skywalker',
+  avatar: 'http://lorempixel.com.br/400/200',
+  city: 'Another Galaxy'
+}
+
 const getUsers = async () => {
   try {
     const { data } = await axios.get(url)
@@ -19,10 +25,30 @@ const getUsers = async () => {
 const addNewUser = async () => {
   try {
     const { data } = await axios.post(url, newUser)
-    if (data === 'User saved!') getUsers()
+    if (!data === 'User saved!') return
+    getUsers()
+    alert(data)
   } catch (error) {
     console.log(error)
   }
+}
+
+const updateAnUser = async () => {
+  try {
+    const { data } = await axios.put(`${url}/2`, updateUser)
+    if (!data === 'User updated!') return
+    getUsers()
+    alert(data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const deleteUser = async () => {
+  const { data } = await axios.delete(`${url}/2`)
+  if (!data === 'User deleted!') return
+  getUsers()
+  alert(data)
 }
 
 window.addEventListener('DOMContentLoaded', getUsers)
