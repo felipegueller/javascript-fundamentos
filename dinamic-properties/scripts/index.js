84a964ffcb1bd692b4a596619ab19cc9cc1d2098
Felipe Gueller <felipe.gueller@gmail.com>
2023-01-11 15:12:21 -0300

    :bug: fix: adding the version with bug

---
+++ dinamic-properties/scripts/index.js
@@ -0,0 +1,17 @@
+const obj = {
+  propA: {
+    propB: 'Deu certo'
+  }
+}
+const keys = ['propA', 'propB']
+const getDinamicAccess = (obj, keys) => {
+  let dinamicValue = obj
+
+  keys.forEach(key => dinamicValue += [key])
+
+  return dinamicValue
+}
+const result = getDinamicAccess(obj, keys)
+
+
+console.log(`O valor obtido foi: ${result}`)
