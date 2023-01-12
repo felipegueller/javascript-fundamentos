dd578184109f3e2c8393f4e4caad1faa558289c9
Felipe Gueller <felipe.gueller@gmail.com>
2023-01-12 09:48:28 -0300

    :recycle: refactor: applying test to verify the original object

--- dinamic-properties/scripts/index.js
+++ dinamic-properties/scripts/index.js
@@ -1,9 +1,13 @@
 const obj = {
   propA: {
     propB: 'Deu certo'
+  },
+  chave1: {
+    chave2: {
+      chave3: 'E agora?'
+    }
   }
 }
-const keys = ['propA', 'propB']
 const getDinamicAccess = (obj, keys) => {
   let dinamicValue = obj
 
@@ -11,7 +15,20 @@ const getDinamicAccess = (obj, keys) => {
 
   return dinamicValue
 }
+
+// 1º teste
+const keys = ['propA', 'propB']
 const result = getDinamicAccess(obj, keys)
 
-
+console.log('Teste 01:')
 console.log(`O valor obtido foi: ${result}`)
+console.log('Objeto: ', obj)
+
+
+// 1º teste
+const keys2 = ['chave1', 'chave2', 'chave3']
+const result2 = getDinamicAccess(obj, keys2)
+
+console.log('Teste 02:')
+console.log(`O valor obtido foi: ${result2}`)
+console.log('Objeto: ', obj)
